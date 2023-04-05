f39e22d1ddfdaa366ebd3d8cfb48e5ee6bd6d122
Lyam Milbury <lyam.milbury@carleton.ca>
2023-04-05 18:54:49 -0400

    [Fix] Added guard clauses in play

--- src/commands/play.js
+++ src/commands/play.js
@@ -13,52 +13,56 @@ module.exports = {
             
     async execute(interaction){
         const userInput = interaction.options.getString('message');
-
         const voiceChannel = interaction.member.voice.channelId;
+        //const serverQueue = queue.get(interaction.guildId);
         
-        //If the user posts a link, use ytdl 
-        if(ytdl.validateURL(userInput)){
-            const song_info = await ytdl.getInfo(userInput)
-            song = {title: song_info.videoDetails.title, url: song_info.videoDetails.video_url}
-        }   else{
-                //Else, user posted a string, use string to search for wanted video
-                const videoFinder = async (query) =>{
-                    const videoResult = await ytSearch(query);
-                    return (videoResult.videos.length > 1) ? videoResult.videos[0]: null;
+        if(voiceChannel === undefined){
+            await interaction.reply('Please join a voice channel to jam.');
+        }
+        else{
+            //If the user posts a link, use ytdl 
+            if(ytdl.validateURL(userInput)){
+                const song_info = await ytdl.getInfo(userInput)
+                song = {title: song_info.videoDetails.title, url: song_info.videoDetails.video_url}
+            }   else{
+                    //Else, user posted a string, use string to search for wanted video
+                    const videoFinder = async (query) =>{
+                        const videoResult = await ytSearch(query);
+                        return (videoResult.videos.length > 1) ? videoResult.videos[0]: null;
+                    }
+
+                    const video = await videoFinder(userInput);
+
+                    if(video){  //Used for ytSearch
+                        song = { title: video.title, url: video.url}
+                    }   else{
+                        await interaction.reply('Error finding video.');
+                    }
                 }
+            
+            const stream = ytdl(song.url, {
+                filter: 'audioonly',
+                quality: 140,
+            });
 
-                const video = await videoFinder(userInput);
+            const player = createAudioPlayer();
+                    
+            const connection = joinVoiceChannel({
+                channelId: interaction.member.voice.channelId,
+                guildId: interaction.guildId,
+                adapterCreator: interaction.guild.voiceAdapterCreator,
+            });
+            
+            const resource = createAudioResource(stream);
+            player.play(resource);
 
-                if(video){  //Used for ytSearch
-                    song = { title: video.title, url: video.url}
-                }   else{
-                    await interaction.reply('Error finding video.');
-                }
-            }
-        
-        const stream = ytdl(song.url, {
-            filter: 'audioonly',
-            quality: 140,
-        });
-
-        const player = createAudioPlayer();
-                
-        const connection = joinVoiceChannel({
-            channelId: interaction.member.voice.channelId,
-            guildId: interaction.guildId,
-            adapterCreator: interaction.guild.voiceAdapterCreator,
-        });
-        
-        const resource = createAudioResource(stream);
-        player.play(resource);
-
-        connection.subscribe(player);
-        //const subscription = connection.subscribe(audioPlayer);
-        await interaction.reply('Song time');
-        player.on(AudioPlayerStatus.Idle, () => {
-            //player.play(getNextResource());
-            setTimeout(() => connection.destroy(), 2_000);
-        });
-
+            connection.subscribe(player);
+            //const subscription = connection.subscribe(audioPlayer);
+            await interaction.reply(`Now jamming to ${song.title} from ${song.url}`);
+            player.on(AudioPlayerStatus.Idle, () => {
+                //player.play(getNextResource());
+                setTimeout(() => connection.destroy(), 2_000);
+            });
+        }
     },
 };
